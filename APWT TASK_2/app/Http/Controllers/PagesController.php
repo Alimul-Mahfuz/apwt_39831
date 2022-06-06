7bce5d00f2a70a85a09f7f4166aae6f618ac6550
Alimul Mahfuz Tushar <74812296+Alimul-Mahfuz@users.noreply.github.com>
2022-06-06 21:59:39 +0600

    Task 2 submitted

---
+++ APWT TASK_2/app/Http/Controllers/PagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class PagesController extends Controller
+{
+    //
+    function home(){
+        return view('welcome');
+    }
+    function viewdetails(){
+        
+        return view('viewdetails');
+    }
+
+}
